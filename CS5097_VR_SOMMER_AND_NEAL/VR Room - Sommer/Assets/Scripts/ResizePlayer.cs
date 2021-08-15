8af876b7508b1779229a9378bde1832f4c9ad389
pongmorrakot <pongmorrakot.t@northeastern.edu>
2021-08-15 16:45:34 -0700

    room decorated

---
+++ CS5097_VR_SOMMER_AND_NEAL/VR Room - Sommer/Assets/Scripts/ResizePlayer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizePlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float scale;
+    public bool small = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void resize()
+    {
+        Vector3 newScale = transform.localScale;
+        if (small)
+        {
+            newScale *= scale;
+        }
+        else
+        {
+            newScale /= scale;
+        }
+        small = !small;
+        transform.localScale = newScale;
+
+    }
+}
